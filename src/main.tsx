d9828b82b55f9323b0fef8c2161e3c39d9d5112c
z927 <46448019+z927@users.noreply.github.com>
2026-04-20 10:58:07 +0200

    feat(theme): add light dark and system theme support

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { queryClient } from "./api";
@@ -6,6 +7,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <App />
+    </ThemeProvider>
   </QueryClientProvider>,
 );
